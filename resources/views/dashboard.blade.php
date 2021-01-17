093cdb9ff0bd817f3bfa09fe07b129bf852bf7a8
jjs9000 <farriszamrie15@gmail.com>
2021-01-18 02:01:31 +0800

    rickroll

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <iframe class="border-0" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1;controls=0;showinfo=0" title="rickroll">
                 </div>
             </div>
         </div>
